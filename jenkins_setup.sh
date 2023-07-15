7177863ad2f4736d09c7cf113b061e2980246acd
Israel <89642129+toba-0x7@users.noreply.github.com>
2023-07-15 23:34:55 +0100

    Create jenkins_setup.sh

---
+++ jenkins_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+sudo apt install ca-certificates -y
+sudo apt install maven git wget unzip -y
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
